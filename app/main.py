db31d01313ab66ee421158858094b94eae8ad340
Josh Schertz <joshschertz3@gmail.com>
2017-06-24 21:12:40 -0400

    Update main.py file with domain checking code and warnings

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
+import os
+
+from domain_checker import domain_checker
+
+dir = os.path.dirname(__file__)
+
 # User submits a url (from a pre-approved source) they want to check
+url = 'https://www.bloomberg.com/news/articles/2017-06-23/senate-holdouts-seek-upper-hand-in-perilous-health-bill-talks'
 
 # The domain is checked against a static list of fake news sites
+domain_clean = domain_checker(url=url)
 
 # The user is made aware immediately if the site is on the list
+if not domain_clean:
+    print('WARNING! The article provided is sourced from %s, which is known '
+          'to provide %s articles.' % (domain_clean[0], domain_clean[1]))
 
 # The article is also requrested from the url provided
 
