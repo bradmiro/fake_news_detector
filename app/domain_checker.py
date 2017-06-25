dab43d98cf08d773e4b6c4f76e53dc3dc49fd89f
Josh Schertz <joshschertz3@gmail.com>
2017-06-24 21:12:04 -0400

    Get domain_checker.py working with flagged_sources.csv file

--- app/domain_checker.py
+++ app/domain_checker.py
@@ -1,24 +1,49 @@
-import re
+import os
+import pandas as pd
+from urllib.parse import urlparse
 
-def checker(url):
+def domain_checker(url):
     """
     :param url: Verify this URL from a loaded list of urls
     :return: True if domain is already flagged, False otherwise.
     """
 
-    url_list = "file_of_url_list"
+    # Open the flagged source file in a pandas dataframe
+    dir = os.path.dirname(__file__)
+    flagged_source_file = os.path.join(dir, 'data', 'flagged_sources.csv')
+    flagged_sources_df = pd.read_csv(flagged_source_file)
 
-    url_re = re.compile('(https:\ / \ /|http:\ / \ /|^)(www.|^)([ ^\ /] * )', re.IGNORECASE)
+    # Parse the domain name out of the provided url
+    parsed_url = urlparse(url)
+    url_domain = '{uri.netloc}'.format(uri=parsed_url)
+    url_domain = url_domain.replace('www.', '')
 
-    url_match = url_re.match(url)
-\
-    with open(url_list) as flagged_urls:
-        for line in flagged_urls.readline():
-            if url_match.group(1) == line:
-                return True
+    print(url_domain)
+    if not url_domain:
+        raise Warning('Unable to determine the root URL for the following '
+                      'file in domain_checker: %s' % (url,))
+        return None
 
-    return False
+    # Search the DF to see if the clean URL has a match
+    flagged_row = flagged_sources_df.loc[flagged_sources_df['url'] == url_domain]
+
+    if len(flagged_row) > 0:
+        type_1 = flagged_row['type'].iloc[0]
+        type_2 = flagged_row['2nd type'].iloc[0]
+        if type_2 == 'nan':
+            return [url_domain, type_1, type_2]
+        else:
+            return [url_domain, type_1]
+    else:
+        return None
 
 
+if __name__ == '__main__':
+    test_url_1 = 'https://www.bloomberg.com/news/articles/2017-06-23/senate-holdouts-seek-upper-hand-in-perilous-health-bill-talks'
+    test_url_2 = 'http://www.zerohedge.com/news/2017-06-24/bernie-sanders-wife-under-fbi-investigation-bank-fraud'
 
+    result_1 = domain_checker(test_url_1)
+    result_2 = domain_checker(test_url_2)
 
+    print(result_1)
+    print(result_2)
